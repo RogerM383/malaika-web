7f2f21bb337dfd0c92ee9c0e22496dfa7fdc864e
MeuK <dev1@metodo10.com>
2021-10-22 13:20:40 +0200

    subtitols

--- src/contexts/apollo/queriesTest.js
+++ src/contexts/apollo/queriesTest.js
@@ -69,6 +69,9 @@ export const GET_VIATGE_BY_SLUG = gql`
                     startCursor
                 }
             }
+            subtitolViatge {
+                subtitolviatge
+            }
             Campsviatge {
                 taxes
                 suplement
